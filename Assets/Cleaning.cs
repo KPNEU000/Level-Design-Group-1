1d82bbd9a0d49d43ff4124a7aba05f15dd9b2c60
Kerem Pauwels <pauwels.k@northeastern.edu>
2026-01-25 17:24:27 -0500

    Cleaning mechanic

---
+++ Assets/Cleaning.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class Cleaning : MonoBehaviour
+{
+    public float range = 10;
+
+    public Animator mopAnimator;
+    public AudioClip mopAudio;
+    public AudioSource cameraAudioSource;
+
+    [Header("HUD")]
+    public TMP_Text clean;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        RaycastingEffect();
+    }
+
+    void RaycastingEffect()
+    {
+        RaycastHit objectHitByRaycast;
+        if (Physics.Raycast(transform.position, transform.forward, out objectHitByRaycast, range))
+        {
+            if (objectHitByRaycast.collider.CompareTag("Mess"))
+            {
+                clean.text = "LMB to Clean";
+                if(Input.GetMouseButtonDown(0))
+                {
+                    if(mopAnimator)
+                    {
+                        mopAnimator.Play("Mop");
+                    }
+                    if (mopAudio) {
+                        cameraAudioSource.PlayOneShot(mopAudio);
+                    }
+                    StartCoroutine("CleanMess", objectHitByRaycast.collider.gameObject);
+                }
+            }
+            else
+            {
+                clean.text = "";
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, transform.forward, Color.red);
+    }
+
+    IEnumerator CleanMess(GameObject mess)
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(mess);
+    }
+}
+
