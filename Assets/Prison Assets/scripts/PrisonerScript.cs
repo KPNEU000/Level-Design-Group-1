36fd7b552b1f0cf8c88408ab8d87b926fec08580
KPNEU000 <pauwels.k@northeastern.edu>
2026-02-18 19:40:01 -0500

    exit section

--- Assets/Prison Assets/scripts/PrisonerScript.cs
+++ Assets/Prison Assets/scripts/PrisonerScript.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class PrisonerScript : MonoBehaviour
 {
     [SerializeField]
@@ -11,12 +13,16 @@ public class PrisonerScript : MonoBehaviour
     public int animType = 0;
     float detectionDistance = 3f;
 
+    private AudioSource audioSource;
+    public List<AudioClip> cries = new List<AudioClip>();
+    public bool soundPlayed = false;
+
     // Start is called before the first frame update
     void Start()
     {
         GameObject player = GameObject.FindWithTag("Player");
         playerTransform = player.transform;
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,11 +31,16 @@ public class PrisonerScript : MonoBehaviour
         if (animType == 0)
         {
             prisonerAnimator.SetBool("IsSitting", true);
+            soundPlayed = false;
         }
 
         if (animType == 1)
         {
             float distance = Vector3.Distance(transform.position, playerTransform.position);
+            if (!soundPlayed) {
+            audioSource.PlayOneShot(cries[Random.Range(0, cries.Count - 1)]);
+            soundPlayed = true;
+            }
             if (distance < detectionDistance)
             {
                 prisonerAnimator.SetBool("IsWaving", true);
