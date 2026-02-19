2b1935a7a7cb9ee7bd13448f9a09f305df48d446
KPNEU000 <pauwels.k@northeastern.edu>
2026-02-18 19:56:26 -0500

    guards

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -1,16 +1,33 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Patrol : MonoBehaviour
 {
+    public GameObject[] waypoints;
+    private NavMeshAgent navMeshAgent;
+    private int destination = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        GoToPoint();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GoToPoint()
     {
-        
+        if (waypoints.Length != 0)
+        {
+            navMeshAgent.destination = waypoints[destination].transform.position;
+            destination = (destination + 1) % waypoints.Length;
+        }
+    }
+
+
+    void Update ()
+    {
+        transform.LookAt(navMeshAgent.destination);
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+            GoToPoint();
     }
 }
