a0269b50bcd277b4654a2ed75890d3f79e11c399
LiquefiedSpam <morganjwillsjr@gmail.com>
2026-03-17 17:06:13 -0400

    added dust effects to graybox 2

---
+++ Assets/ParticleEnterPlayer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParticleEnterPlayer : MonoBehaviour
+{
+
+    ParticleSystem ps;
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+    void OnParticleTrigger()
+    {
+        Debug.Log("hi");
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            if (p.remainingLifetime > p.startLifetime / 8f)
+                p.remainingLifetime = p.startLifetime / 8f;
+
+
+            enter[i] = p;
+        }
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+    }
+}
