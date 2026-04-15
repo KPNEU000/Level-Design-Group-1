9b5579342ef96a92d8908ffcb720a17e88e8320d
LiquefiedSpam <morganjwillsjr@gmail.com>
2026-04-14 21:51:40 -0400

    animStuff

--- Assets/Final/Scripts/SoldierController.cs
+++ Assets/Final/Scripts/SoldierController.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class SoldierController : MonoBehaviour
@@ -8,14 +9,26 @@ public class SoldierController : MonoBehaviour
     [Header("Side")]
     public DialogueSide mySide;
 
+    private Animator anim;
+
     private void OnEnable()
     {
         DialogueManager.OnDialogueTriggered += OnDialogue;
+        anim = GetComponent<Animator>();
+        SetAnim("IsStandAimIdle");
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         DialogueManager.OnDialogueTriggered -= OnDialogue;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot("MEDIUM");
+        }
     }
 
     void OnDialogue(DialogueType type, DialogueSide incomingSide)
@@ -48,8 +61,23 @@ public class SoldierController : MonoBehaviour
     void Shoot(string level)
     {
         Debug.Log(name + " fires on " + level + " dialogue!");
+        SetAnim("IsStandFire");
+
 
         // placeholder for stuff like anims basically feel free to be creative here ig
 
+    }
+
+    void SetAnim(string name)
+    {
+        anim.SetBool("IsStandFire", false);
+        anim.SetBool("IsStandAimIdle", false);
+
+        anim.SetBool(name, true);
+    }
+
+    void EndStandShoot()
+    {
+        SetAnim("IsStandAimIdle");
     }
 }
