50b5ee999e8315e03f6b1256294d5dbbf5d27594
LiquefiedSpam <morganjwillsjr@gmail.com>
2026-04-23 15:40:40 -0400

    working on bullets

--- Assets/Final/Scripts/PaperBehavior.cs
+++ Assets/Final/Scripts/PaperBehavior.cs
@@ -1,13 +1,24 @@
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PaperBehavior : MonoBehaviour
 {
 
+    private MaterialPropertyBlock propBlock;
+    private Renderer rend;
+    private float highSetting = 10;
+    private float baseSetting = 0;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameManager.Ins.OnPapersGrounded += Illuminate;
+        propBlock = new MaterialPropertyBlock();
+        rend = GetComponent<Renderer>();
+        propBlock.SetFloat("_SelfIllumination", baseSetting);
+        //rend.SetPropertyBlock(propBlock);
     }
 
     void OnDisable()
@@ -31,6 +42,8 @@ public class PaperBehavior : MonoBehaviour
 
     public void Illuminate()
     {
-
+        Debug.Log("illuminate papers");
+        propBlock.SetFloat("_SelfIllumination", highSetting);
+        rend.SetPropertyBlock(propBlock);
     }
 }
--- Assets/Final/Scripts/PaperShatterScript.cs
+++ Assets/Final/Scripts/PaperShatterScript.cs
@@ -5,6 +5,7 @@ public class PaperShatterScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OnShatterEnd()
     {
+        Debug.Log("paper shattered");
         GameManager.Ins.OnShatterEnd();
     }
 }
